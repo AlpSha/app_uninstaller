3ab8b648d191e40ea6749e255975c65704fc1c98
Fahmi <fahmi@ekrut.com>
2022-03-11 13:32:21 +0700

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.4
+
+* Now you can uninstall android app in flutter
---
+++ example/android/app/src/main/java/com/devash/uninstall_apps_example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.devash.uninstall_apps_example;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+public class MainActivity extends FlutterActivity {
+    
+    @Override
+   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        
+    }
+}
